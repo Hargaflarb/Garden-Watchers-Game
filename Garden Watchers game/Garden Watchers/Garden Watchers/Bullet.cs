41d203d407cfccff4af669e8cda802cf6de949df
Hvemvilhvad <malthewbirch@gmail.com>
2024-11-04 13:03:36 +0100

    fix

--- Garden Watchers game/Garden Watchers/Garden Watchers/Bullet.cs
+++ Garden Watchers game/Garden Watchers/Garden Watchers/Bullet.cs
@@ -1,25 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Garden_Watchers
 {
-    internal class Bullet:GameObject
+    internal class Bullet : GameObject
     {
-        int damage = 3;
-        public Bullet() 
+        private int damage = 3;
+
+        public Bullet() :base()
         { 
             
         }
 
-        public override void Update()
+        protected override void Initialize()
+        {
+
+        }
+
+
+        public override void Update(GameTime gameTime, Vector2 screenSize)
         {
             
         }
 
-        public override void LoadContent()
+        public override void LoadContent(ContentManager content)
         {
             
         }
--- Garden Watchers game/Garden Watchers/Garden Watchers/EnemyBullet.cs
+++ Garden Watchers game/Garden Watchers/Garden Watchers/EnemyBullet.cs
@@ -1,21 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Garden_Watchers
 {
     internal class EnemyBullet : GameObject
     {
-        public override void LoadContent()
+
+        protected override void Initialize()
         {
-            
+
         }
 
-        public override void Update()
+
+        public override void Update(GameTime gameTime, Vector2 screenSize)
         {
-            
+
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+
         }
     }
 }
