0effac31cdc5180c2a81d88cdc020d3c7c549e05
Hargaflarb <emmabrocher@gmail.com>
2024-11-04 12:47:40 +0100

    added forgotten classes

---
+++ Garden Watchers game/Garden Watchers/Garden Watchers/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garden_Watchers
+{
+    internal class Bullet:GameObject
+    {
+        int damage = 3;
+        public Bullet() 
+        { 
+            
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void LoadContent()
+        {
+            
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+            
+        }
+    }
+}
---
+++ Garden Watchers game/Garden Watchers/Garden Watchers/EnemyBullet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garden_Watchers
+{
+    internal class EnemyBullet : GameObject
+    {
+        public override void LoadContent()
+        {
+            
+        }
+
+        public override void Update()
+        {
+            
+        }
+    }
+}
