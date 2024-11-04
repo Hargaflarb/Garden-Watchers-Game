ee0d12b2cd82698fe60e9abb96c7eafbf31fecd1
Eleavate <echoovervad@gmail.com>
2024-11-04 11:15:44 +0100

    Move method

--- Garden Watchers game/Garden Watchers/Garden Watchers/Character.cs
+++ Garden Watchers game/Garden Watchers/Garden Watchers/Character.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace Garden_Watchers
 {
+    /// <summary>
+    /// The Character class which Enemy and Player uses
+    /// </summary>
     internal abstract class Character : GameObject
     {
+        //Fields
+        protected Vector2 position = Vector2.Zero;
+        protected Vector2 velocity;
+        protected float speed;
+
+        //Properties
+        
+
+        //Methods
+        protected void Move(GameTime gameTime, Vector2 screenSize)
+        {
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            position += ((velocity * speed) * deltaTime);
+        }
+
     }
 }
