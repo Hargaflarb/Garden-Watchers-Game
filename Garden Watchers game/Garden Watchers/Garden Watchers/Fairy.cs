b4414a9b2101d59b066a0d60be911899dbda5613
Eleavate <echoovervad@gmail.com>
2024-11-07 10:16:30 +0100

    Constructor

--- Garden Watchers game/Garden Watchers/Garden Watchers/Fairy.cs
+++ Garden Watchers game/Garden Watchers/Garden Watchers/Fairy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace Garden_Watchers
 {
     internal class Fairy : Enemy
     {
+        public Fairy(int health, Vector2 position, float speed) : base(health, position, speed)
+        {
+        }
     }
 }
--- Garden Watchers game/Garden Watchers/Garden Watchers/Flamingo.cs
+++ Garden Watchers game/Garden Watchers/Garden Watchers/Flamingo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,8 @@ namespace Garden_Watchers
     internal class Flamingo : Enemy
     {
         // *boom!*
+        public Flamingo(int health, Vector2 position, float speed) : base(health, position, speed)
+        {
+        }
     }
 }
